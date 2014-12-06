09599f3ba1ea1bab1e710cc7d018782c2a6500ba
Damien BUTY <dam.buty@gmail.com>
2014-12-06 21:22:02 +0100

    Jour 2

---
+++ www/js/bissey.js
@@ -0,0 +1,14 @@
+
+(function() {
+$(document).ready(function(){
+
+$("#double-border>div").click(function() {
+    $("#double-border").addClass("blur");
+    $("#modal").css("opacity", 1);
+});
+
+
+
+
+});
+})();
