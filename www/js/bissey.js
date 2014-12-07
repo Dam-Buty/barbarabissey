5d1d47861da27725b650117ca19064454e39fe56
Damien BUTY <dam.buty@gmail.com>
2014-12-07 18:43:39 +0100

    Intro OK

--- www/js/bissey.js
+++ www/js/bissey.js
@@ -2,13 +2,27 @@
 (function() {
 $(document).ready(function(){
 
-$("#double-border>div").click(function() {
-    $("#double-border").addClass("blur");
-    $("#modal").css("opacity", 1);
+mixpanel.track("Arrive");
+
+$("#double-border h2").hover(function() {
+    $("#double-border").addClass("desature");
+}, function() {
+    $("#double-border").removeClass("desature");
 });
 
+$("#double-border h2").click(function() {
+    $("#double-border").removeClass("desature");
+    $("#double-border").addClass("blur");
+    $("#double-border h2").addClass("inactive");
+    
+    $("#modal-" + $(this).parent().attr("id")).css("opacity", 1).css("z-index", 100);
+});
 
-
+$(".close-button").click(function() {
+    $("#double-border").removeClass("blur");
+    $(".modal").css("opacity", 0).css("z-index", 10);
+    $("#double-border h2").removeClass("inactive");
+});
 
 });
 })();
