e7b6d139dc8e47bc79c2824ca487ee0c7177642f
Damien BUTY <dam.buty@gmail.com>
2014-12-07 20:35:25 +0100

    Intro OK animations FF OK

--- www/js/bissey.js
+++ www/js/bissey.js
@@ -4,24 +4,35 @@ $(document).ready(function(){
 
 mixpanel.track("Arrive");
 
-$("#double-border h2").hover(function() {
+var modal = false;
+
+$("#double-border>h2").hover(function() {
     $("#double-border").addClass("desature");
+    $("#double-border>h2").not($(this)).addClass("inactive");
 }, function() {
     $("#double-border").removeClass("desature");
+    $("#double-border>h2").addClass("instant");
+    
+    if (!modal) {
+        $("#double-border>h2").removeClass("inactive");
+    }
 });
 
 $("#double-border h2").click(function() {
     $("#double-border").removeClass("desature");
     $("#double-border").addClass("blur");
-    $("#double-border h2").addClass("inactive");
+    $("#double-border>h2").addClass("inactive");
     
-    $("#modal-" + $(this).parent().attr("id")).css("opacity", 1).css("z-index", 100);
+    $("#modal-" + $(this).attr("id")).css("opacity", 1).css("z-index", 100);
+    modal = true;
 });
 
 $(".close-button").click(function() {
     $("#double-border").removeClass("blur");
     $(".modal").css("opacity", 0).css("z-index", 10);
     $("#double-border h2").removeClass("inactive");
+    
+    modal = false;
 });
 
 });
