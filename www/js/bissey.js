d57907b679aee47022a242a8adec4f9ab1d5e17a
Damien BUTY <dam.buty@gmail.com>
2014-12-11 17:37:06 +0100

    Interface OK manque backend

--- www/js/bissey.js
+++ www/js/bissey.js
@@ -27,6 +27,58 @@ $("#double-border h2").click(function() {
     modal = true;
 });
 
+$(".to-contact").click(function() {
+    $("#double-border").removeClass("desature");
+    $("#double-border").addClass("blur");
+    $("#double-border>h2").addClass("inactive");
+    $(".modal").css("opacity", 0).css("z-index", 10);
+    
+    $("#modal-contact").css("opacity", 1).css("z-index", 100);
+    
+    var parent = $(this).parent();
+    var select = $("#modal-contact select");
+    var textarea = $("#modal-contact textarea");
+    
+    if (parent.hasClass("modal")) {
+        var id = parent.attr("id");
+        
+        switch(id) {
+            case "modal-conseil-image":
+                select.val("image");
+                textarea.attr("placeholder", "Parlez nous de votre profil, de votre style actuel, et de vos objectifs...");
+                break;
+            case "modal-personal-shopper":
+                select.val("shopper");
+                textarea.attr("placeholder", "Parlez nous un peu de vous! Vos envies, votre style et votre budget...");
+                break;
+            case "modal-conseil-cadeau":
+                select.val("cadeau");
+                textarea.attr("placeholder", "A qui s'adresse votre cadeau? Quels sont ses goûts, ses centres d'intérêt?");
+                break;
+        }
+    } else {
+        select.val("");
+        textarea.attr("placeholder", "Votre message...");
+    }
+    
+    modal = true;
+});
+
+$("#modal-contact select").change(function() {
+    var textarea = $("#modal-contact textarea");
+    switch($(this).val()) {
+        case "image":
+            textarea.attr("placeholder", "Parlez nous de votre profil, de votre style actuel, et de vos objectifs...");
+            break;
+        case "shopper":
+            textarea.attr("placeholder", "Parlez nous un peu de vous! Vos envies, votre style et votre budget...");
+            break;
+        case "cadeau":
+            textarea.attr("placeholder", "A qui s'adresse votre cadeau? Quels sont ses goûts, ses centres d'intérêt?");
+            break;
+    }
+});
+
 $(".close-button").click(function() {
     $("#double-border").removeClass("blur");
     $(".modal").css("opacity", 0).css("z-index", 10);
